cc77e10bb71ba2d96b16b4447bee425061557356
envelope <106300938+TkachevNikita@users.noreply.github.com>
2024-08-04 22:51:35 +0500

    fix: edit json path

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@
  * @returns {Promise<Response>}
  */
 export const fetchData = async () => {
-    return await fetch('/mockCv.json');
+    return await fetch('/front-end-entrance-exam/mockCv.json');
 };
 
 /**
